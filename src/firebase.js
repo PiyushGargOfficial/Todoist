94f0a32a4e620e88f495cbe60c19d146edcb9929
Piyush <piyushgargofficial@gmail.com>
2022-02-27 14:00:25 -0500

    Initial Setup Added

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = firebase.initialize({
+  apikey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectID: "",
+  storageBucket: "",
+  messageSenderID: "",
+  apiID: "",
+});
+
+export { firebaseConfig as firebase };
